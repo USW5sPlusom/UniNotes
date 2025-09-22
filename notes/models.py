9daa3548f356e22999edd4a72eddd9dbc18e8614
Aleksey <vaznoe2024@mail.ru>
2025-09-22 11:17:07 +0300

    v.2 Добавлена модель конспекта

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Введите заголовок')
+    content = models.TextField(verbose_name='Введите текст', blank= True)
+    subtag = models.CharField(max_length=200, verbose_name='Предмет')
+
+    class Meta:
+        pass
